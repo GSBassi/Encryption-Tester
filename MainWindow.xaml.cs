1f9764f3d85b4658ccd27f9626288a67d10dba70
Phil Johnson <philjohnson3@gmail.com>
2014-06-07 07:41:16 -0700

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptionTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            PopulateComboBoxes();
+        }
+
+        void PopulateComboBoxes()
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                cmbAsym.DataContext = SharedMethods.GetASymmetricTypes();
+                cmbSym.DataContext = SharedMethods.GetSymmetricTypes();
+                //If cmboInput Is Nothing Then Exit Sub
+                //If Not SelectedConversionType.HasValue Then Exit Sub
+                //grdNormalInput.Visibility = Windows.Visibility.Collapsed
+                //ctlGPS.Visibility = Windows.Visibility.Collapsed
+                //'Me.cmboInput.Items.Clear()
+                //Dim m_Convo As ConvertObject = ConversionLogic.ReturnConvoGroupType(SelectedConversionType)
+                //If m_Convo IsNot Nothing Then
+                //    grdNormalInput.Visibility = Windows.Visibility.Visible
+
+                //    Dim sStringCol As New Specialized.StringCollection
+                //    sStringCol = m_Convo.ConvertTypes
+                //    cmboInput.DataContext = sStringCol
+                //ElseIf SelectedConversionType = ConversionLogic.ConversionTypes.GPS Then
+                //    ctlGPS.Visibility = Windows.Visibility.Visible
+                //    ctlGPS.ClearValues()
+                //    txtInput.Text = String.Empty
+
+                //End If
+                cmbAsym.SelectedIndex = 0;
+                cmbSym.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                //ExceptionHandler.HandleException(ex)
+            }
+            finally
+            {
+                Mouse.OverrideCursor = Cursors.Arrow;
+            }
+        }
+
+        private void btnConvertsym_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(symInput.Text))
+                {
+                    SharedMethods.HashType hshTyp = (SharedMethods.HashType)Enum.Parse(typeof(SharedMethods.HashType), cmbSym.SelectedValue.ToString());
+                    symOutput.Text = SharedMethods.ComputeHash(symInput.Text, hshTyp);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void btnConvertAsym_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if ((!string.IsNullOrEmpty(asymInput.Text)) && (!string.IsNullOrEmpty(txtPass.Text)))
+                {
+                    asymOutput.Text = SharedMethods.Encrypt(asymInput.Text, txtPass.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+    }
+}
---
+++ SharedMethods.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionTester
+{
+    internal class SharedMethods
+    {
+
+        #region "Asymmetric"
+
+        internal enum AsymType
+        {
+            TripleDES
+        }
+
+        internal static ReadOnlyCollection<string> GetASymmetricTypes()
+        {
+            List<string> theReturn = new List<string>();
+            foreach (AsymType s in Enum.GetValues(typeof(AsymType)))
+            {
+                theReturn.Add(s.ToString());
+            }
+            return new ReadOnlyCollection<string>(theReturn);
+        }
+
+        /// <summary>
+        /// This method encrypts a string.
+        /// </summary>
+        /// <param name="uncoded">the text to be encrypted</param>
+        /// <param name="password">the private key in string format</param>
+        /// <returns>note that it takes unicode text, and coverts it's encryption to base64</returns>
+        /// <remarks></remarks>
+        public static string Encrypt(string uncoded, string password)
+        {
+            byte[] plainTextByteArray = System.Text.Encoding.Unicode.GetBytes(uncoded);
+            byte[] theOutput = null;
+
+            using (ICryptoTransform enCryp = GetCryptProvider(password).CreateEncryptor())
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cryptStrm = new CryptoStream(ms, enCryp, CryptoStreamMode.Write))
+                    {
+                        cryptStrm.Write(plainTextByteArray, 0, plainTextByteArray.Length);
+                        cryptStrm.FlushFinalBlock();
+                    }
+                    theOutput = ms.ToArray();
+                }
+            }
+
+            return Convert.ToBase64String(theOutput);
+        }
+
+        private static TripleDESCryptoServiceProvider GetCryptProvider(string thePassword)
+        {
+            //Dim theAlgo As RijndaelManaged = New RijndaelManaged            
+            TripleDESCryptoServiceProvider theAlgo = new TripleDESCryptoServiceProvider();
+            byte[] salt1 = new byte[8];
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                // Fill the array with a random value.
+                rngCsp.GetBytes(salt1);
+            }
+            using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(thePassword, salt1))
+            {
+                theAlgo.Key = key.GetBytes(Convert.ToInt32(theAlgo.KeySize / 8));
+                theAlgo.IV = key.GetBytes(Convert.ToInt32(theAlgo.BlockSize / 8));
+            }
+            return theAlgo;
+        }
+
+
+
+        #endregion
+
+
+        #region "Symmetric"
+        internal enum HashType
+        {
+            SHA1,
+            SHA256,
+            SHA384,
+            SHA512,
+            SHA1Crypto
+        }
+
+        internal static ReadOnlyCollection<string> GetSymmetricTypes()
+        {
+            List<string> theReturn = new List<string>();
+            foreach (HashType s in Enum.GetValues(typeof(HashType)))
+            {
+                theReturn.Add(s.ToString());
+            }
+            return new ReadOnlyCollection<string>(theReturn);
+        }
+
+        /// <summary>
+        ///  Generates a hash for the given plain text value and returns a 
+        /// base64-encoded result. 
+        /// </summary>
+        /// <param name="plainText">Plaintext value to be hashed. The function does not check 
+        /// whether this parameter is null.</param>
+        /// <param name="theHashAlgorithm">  Name of the hash algorithm. Allowed values are: 
+        /// "MD5", "SHA1", "SHA256", "SHA384", and "SHA512" (if any other value is specified 
+        /// MD5 hashing algorithm will be used). This value is case-insensitive.</param>
+        /// <returns>Hash value formatted as a base64-encoded string.</returns>
+        /// <remarks></remarks>
+        internal static string ComputeHash(string plainText, HashType theHashAlgorithm)
+        {
+            // If salt is not specified, generate it on the fly.
+            byte[] plainTextBytes = null;
+            HashAlgorithm hash = null;
+            byte[] hashBytes = null;
+            string hashValue = null;
+
+            // Convert plain text into a byte array.
+            plainTextBytes = System.Text.Encoding.Unicode.GetBytes(plainText);
+            try
+            {
+                // Initialize appropriate hashing algorithm class.
+                switch (theHashAlgorithm)
+                {
+                    case HashType.SHA1:
+                        hash = new SHA1Managed();
+                        break;
+                    case HashType.SHA256:
+                        hash = new SHA256Managed();
+                        break;
+                    case HashType.SHA384:
+                        hash = new SHA384Managed();
+                        break;
+                    case HashType.SHA512:
+                        hash = new SHA512Managed();
+                        break;
+                    case HashType.SHA1Crypto:
+                        //the ONLY FIPS compliant one here
+                        hash = new SHA1CryptoServiceProvider();
+                        break;
+                    default:
+                        hash = new MD5CryptoServiceProvider();
+                        break;
+                }
+
+                // Compute hash value of our plain text with appended salt.
+                hashBytes = hash.ComputeHash(plainTextBytes);
+
+                // Convert result into a base64-encoded string.
+                hashValue = Convert.ToBase64String(hashBytes);
+            }
+            finally
+            {
+                if (hash != null)
+                {
+                    hash.Dispose();
+                }
+            }
+            // Return the result.
+            return hashValue;
+        }
+        #endregion
+
+    }
+}
